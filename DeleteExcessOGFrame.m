57a349522f7b0c8d62e9a167a3cba396250d7309
Atif Mahmud <atifmahmud95@gmail.com>
2017-12-16 08:31:18 +0600

    Works

---
+++ DeleteExcessOGFrame.m
@@ -0,0 +1,11 @@
+%% Delete the frames we no longer need
+for num= 1:1358 
+    file=strcat('frame_number', num2str(num), '.png');
+    delete (file);
+end 
+
+for num= 1700:3081 
+    file=strcat('frame_number', num2str(num), '.png');
+    delete (file);
+end 
+
